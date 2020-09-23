159a230706f6485588a4a2a3195318acdbf2569d
ferraretto <ferraretto@CPD>
2020-09-23 15:32:46 -0300

    Commit inicial javafullstack

---
+++ PrimeiroProjeto/src/cursojava/classes/Aluno.java
@@ -0,0 +1,27 @@
+package cursojava.classes;
+
+public class Aluno {
+
+	String nome;
+	int idade;
+	String dataNascimento;
+	String registroGeral;
+	String numeroCpf;
+	String nomeMae;
+	String nomePai;
+	String dataMatricula;
+	String nomeEscola;
+	String serieMatriculado;
+
+	public Aluno() {
+		
+	}
+	
+	public Aluno (String nomePadrao) {
+		nome = nomePadrao;
+	}
+	public Aluno (String nomePadrao, int idadePadrao) {
+		nome = nomePadrao;
+		idade = idadePadrao;
+	}
+}
---
+++ PrimeiroProjeto/src/cursojava/executavel/oneclass.java
@@ -0,0 +1,19 @@
+package cursojava.executavel;
+
+import cursojava.classes.Aluno;
+
+public class oneclass {
+	/*Main é um método auto executável em Java*/
+	public static void main(String[] args) {
+		
+		Aluno aluno1 = new Aluno();
+		
+		Aluno aluno2 = new Aluno();
+	
+		Aluno aluno3 = new Aluno();
+		
+		Aluno aluno4 = new Aluno("Maria");
+		
+		
+	}
+}
